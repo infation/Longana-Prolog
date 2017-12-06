63986d29e75f92e168352d0c6a9dd342a7c4af8d
sminev@ramapo.edu <sminev@ramapo.edu>
2017-12-05 23:05:06 -0500

    Added a user input for the side of which the tile to be played. Created the needed rules in order to place it both left and right. It works with double tiles and if the previous player has passed(manually checking it right now, since I haven't implemented the computer predicates.

--- longana.pl
+++ longana.pl
@@ -1,15 +1,29 @@
 %Ask the user for tournament score in the beginning of the tournament
 ask_tscore(X) :-
     write('Enter Tournament Score (1-500): '), 
-    read(N),
+    read(N), nl,
     integer(N),
     validate(N, 500),
-    X = N, 
-    nl.
+    X = N.
+
 
 ask_tscore(X) :-
-    write('Wrong input. '), nl,
+    write('Wrong tournamen score input. '), nl,
     ask_tscore(X).
+
+ask_side_placement(X) :-
+    write('Do you want to place it on the (left/right)? : '),
+    read(N), nl,
+    validate_side(N),
+    X = N.
+
+ask_side_placement(_) :-
+    write('Wrong side placement. '), nl,
+    ask_side_placement(_).   
+
+ 
+validate_side(left).
+validate_side(right).
 
 %Ask the user to choose a tile from his/her hand
 choose_tile(X, Hhand) :-
@@ -17,24 +31,26 @@ choose_tile(X, Hhand) :-
     length(Hhand, Length),
     write(Length),
     write('): '), 
-    read(N),
+    read(N), nl,
     integer(N),
     validate(N, Length),
-    X is N-1, 
-    nl.
+    X is N-1.
 
 
 choose_tile(X, Hhand) :- 
-    write('Wrong input. '), nl,
+    write('Wrong tile input. '), nl,
     choose_tile(X, Hhand).
 
 validate(N, End) :- 
     N >= 1,  
     N =< End.
 
+
+
+
 %To print the tile's contents
 print_tile([]).
-print_tile([First, Second | Rest]) :- 
+print_tile([First, Second | _]) :- 
     write(First), 
     write('-'), 
     write(Second), 
@@ -63,6 +79,9 @@ print_state(Hhand, Chand, Deck, Board) :-
     print_list(Deck), nl.
 
 
+
+
+
 %Initialize and shuffle the deck
 init_deck(Deck) :- 
     D = [[0,0],[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],
@@ -79,6 +98,7 @@ draw(Tile, [First | Rest], NewDeck):-
     Tile = First,
     NewDeck = Rest.
 
+
 %Initialize the hands of the players
 init_hands([], [], _ , 0).
 init_hands([First | HNewRest], [Second | CNewRest], [First, Second | Rest], N) :-
@@ -93,12 +113,20 @@ init_game(Hhand, Chand, Deck, Tscore):-
     length(X, 16),
     append(X, Deck, D).
 
+
+
+
 %To add a tile in a list
-add_tile_left(List, Tile, [Tile|List]).
-add_tile_right(List, Tile, NewList):-
+add_tile(List, left, Tile, [Tile|List]).
+add_tile(List, right, Tile, NewList):-
     append(List, [Tile], NewList).
+add_tile(List, _, Tile, [Tile|List]).
+
+
 
 switch_pips([First|[Second|_]], [Second|[First]]).
+is_double([Pip1,[Pip2|_]]):- Pip1 = Pip2.
+
 
 %Remove tile at a specified index
 remove_tile_at([],_,[]) :- write('Index out of bounds'), nl.
@@ -111,7 +139,12 @@ remove_tile_at([First | Rest], N , [First | NewRest]) :-
 get_tile_at(Tile, Index, List):-
     nth0(Index, List, Tile).
 
-check_placement(Tile, left, Board, NewTile) :-
+
+
+
+
+
+check_placement(Tile, _, Board, NewTile) :-
     length(Board, L),
     L = 0,
     NewTile = Tile.
@@ -141,17 +174,57 @@ check_placement([Pip1|[Pip2|_]], right, Board, NewTile) :-
     Second = Pip2,
     switch_pips([Pip1|[Pip2]], NewTile).
 
+
+
+
+
+check_both_sides(Tile, Board):- check_placement(Tile, _, Board, _).
+
+
+
+
+
+
+is_move_possible(Tile, _, Board, IsPassed):-
+    IsPassed,
+    check_both_sides(Tile, Board).
+
+is_move_possible(Tile, _, Board, _):-
+    is_double(Tile),
+    check_both_sides(Tile, Board).
+
+is_move_possible(Tile, Side, Board, _):-
+    check_placement(Tile, Side, Board, _).
+
+
+
+
+
+
+
+
+
 human_play(Hhand, Board, NewHand, NewBoard):-
     choose_tile(Index, Hhand),
-    %NewIndex is Index - 1,
     get_tile_at(Tile, Index , Hhand),
-    check_placement(Tile, left, Board, NewTile),
+    IsPassed = true,
+    is_move_possible(Tile, left, Board, IsPassed),
+    ask_side_placement(Side),
+    check_placement(Tile, Side, Board, NewTile),
     remove_tile_at(Hhand, Index, NewHand),
-    add_tile_left(Board, NewTile, NewBoard).
+    add_tile(Board, Side, NewTile, NewBoard).
 
 %human_play(Hhand, Board, NewHand, NewBoard):-
 %    write('Invalid move.'), nl,
 %    human_play(Hhand, Board, NewHand, NewBoard).
+
+
+
+
+
+
+
+
 
 round(Hhand, Chand, Deck, Tscore) :- 
     init_game(Hhand, Chand, Deck, Tscore), 
@@ -165,7 +238,7 @@ round(Hhand, Chand, Deck, Board, Tscore, N) :-
     NewN is N+1,
     round(NewHand, Chand, Deck, NewBoard, Tscore, NewN).
 
-?- round(Hhand, Chand, Deck, Tscore). 
+?- round(_,_,_,_). 
 
    %halt.
 
